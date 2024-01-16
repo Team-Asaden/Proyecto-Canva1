8c8d243ca178094e46c41ccf4ff7d4df613f7a7e
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-01-16 11:13:29 -0600

    app.js : recuadros añadidos, index.html:canvas actualizado

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var myCanvas = document.getElementById("myCanvas");
+var context = myCanvas.getContext("2d");
+
+context.fillRect(5, 5, 145, 145);
+context.fillStyle = "rgb(0, 162, 232)";
+context.fillRect(40, 55, 145, 145);
+context.fillStyle = "rgba(255, 0, 0, 0.2)";
+context.fillRect(75, 105, 145, 145);
