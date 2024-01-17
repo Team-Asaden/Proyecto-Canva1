b46b1a9124aadf3c738f0b2a06713c3a6b45671c
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-01-17 09:40:07 -0600

    notas colocadas en una carpeta

--- js/app.js
+++ js/app.js
@@ -6,19 +6,4 @@ context.fillStyle = "rgb(0, 162, 232)";
 context.fillRect(40, 55, 145, 145);
 context.fillStyle = "rgba(255, 0, 0, 0.2)";
 context.fillRect(75, 105, 145, 145);
-
-// context.beginPath();
-// context.strokeStyle = "rgb(0, 0, 0)"; 
-// context.arc(123, 93, 70, 0, 1.5 * Math.PI, true);
-// context.stroke();
-
-// context.beginPath();
-// context.fillStyle = "rgb(0, 0, 0)";
-// context.arc(123, 93, 70, 0, 1.2 * Math.PI, true);
-// context.fill();
-
-// context.lineWidth = 20;
-// context.beginPath();
-// context.moveTo(5, 50);
-// context.bezierCurveTo(30, 30, 130, 530, 200, 100);
-// context.stroke();
+ 
