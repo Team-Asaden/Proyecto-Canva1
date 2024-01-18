94727a710a80d83f317e3a0304653b29237e21e8
Marco Alonso <marenas@itsmarts.com.mx>
2024-01-18 14:33:36 -0600

    Cubo funcional

--- js/app.js
+++ js/app.js
@@ -4,13 +4,11 @@ var ctx = canva.getContext("2d");
 
 let x=400,y=200,w=50,h=50;
 ctx.fillStyle = "blue";
-ctx.strokeStyle = "green";
 ctx.fillRect(x,y,w,h);
 
 const azul = () => {
     ctx.fillStyle = "blue";
     ctx.fillRect(x,y,w,h);
-    console.log(ctx.fillStyle)
 }
 
 const rojo = () => {
@@ -22,34 +20,71 @@ const verde = () => {
     ctx.fillStyle = "green";
     ctx.fillRect(x,y,w,h);
 }
-const arriba = () => {
-    y = y -35;
-    ctx.fillRect(x,y,w,h);
-    ctx.clearRect(x,y+50,w,h);
-}
-const abajo  = () => {
-    y = y +35;
-    ctx.fillRect(x,y,w,h)
-    ctx.clearRect(x,y-50,w,h);
-}
-const izquierda = () => {
-    x = x - 35;
-    ctx.fillRect(x,y,w,h)
-    ctx.clearRect(x+50,y,w,h);
-}
-const derecha = () => {
-    x = x + 35;
-    ctx.fillRect(x,y,w,h)
-    ctx.clearRect(x-50,y,w,h);
+
+const draw_p1 = () =>{
+
+    ctx.moveTo(120,25);
+    ctx.fillStyle = '200 0 0'
+    ctx.fillRect(x, y, w, h);
+   
+   }
+const del_p1 = () =>{
+    ctx.clearRect(0, 0, canva.width, canva.height);
 }
 
+window.onkeydown = function(event) {
+    var num = event.keyCode;
+    event.preventDefault();
+    if (num == 87) { // 'W'
+     y -= 35;
+    } else if (num == 83) { // 'S'
+     y += 35;
+    } else if (num == 65) { // 'A'
+      x -= 35;
+    } else if (num == 68) { // 'D'
+      x += 35;
+    }
+    del_p1();
+    draw_p1();
+  }
+function draw() {
 
-
+    if (canva.getContext) {
+  
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
+    }
+  }
 document.getElementById('blue').addEventListener('click',azul);
 document.getElementById('red').addEventListener('click',rojo);
 document.getElementById('green').addEventListener('click',verde);
 
-document.getElementById('top').addEventListener("click",arriba)
-document.getElementById('bottom').addEventListener("click",abajo)
-document.getElementById('left').addEventListener("click",izquierda)
-document.getElementById('right').addEventListener("click",derecha)
+
+    // const arriba = () => {
+    //     y = y -35;
+    //     ctx.fillRect(x,y,w,h);
+    //     ctx.clearRect(x,y+50,w,h);
+    // }
+    // const abajo  = () => {
+    //     y = y +35;
+    //     ctx.fillRect(x,y,w,h)
+    //     ctx.clearRect(x,y-50,w,h);
+    // }
+    // const izquierda = () => {
+    //     x = x - 35;
+    //     ctx.fillRect(x,y,w,h)
+    //     ctx.clearRect(x+50,y,w,h);
+    // }
+    // const derecha = () => {
+    //     x = x + 35;
+    //     ctx.fillRect(x,y,w,h)
+    //     ctx.clearRect(x-50,y,w,h);
+    // }
+    
+// document.getElementById('top').addEventListener("click",arriba)
+// document.getElementById('bottom').addEventListener("click",abajo)
+// document.getElementById('left').addEventListener("click",izquierda)
+// document.getElementById('right').addEventListener("click",derecha)
